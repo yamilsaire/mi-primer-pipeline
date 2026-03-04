e558fbae10e94d1a79435ec628a0060f6dcccbd2
Yamil Saire <tu-correo@ejemplo.com>
2026-03-04 09:39:53 -0500

    Mi primer pipeline configurado

---
+++ app.js
@@ -0,0 +1,4 @@
+function saludar(nombre) {
+  return `Hola, ${nombre}!`;
+}
+module.exports = saludar;
---
+++ test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+const saludar = require('../app');
+describe('Prueba de la función saludar', () => {
+  it('Debería saludar a Juan', () => {
+    assert.strictEqual(saludar('Juan'), 'Hola, Juan!');
+  });
+});
